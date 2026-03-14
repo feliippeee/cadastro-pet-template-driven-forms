2efd97e6fbe1f628f976f4af05c18efc05e58fd9
feelipefreitas <feelipe.freitas97@gmail.com>
2026-03-14 20:46:01 -0300

    Criando o validador customizado do abrigo

--- src/app/features/pet-informations/pet-form/pet-form.ts
+++ src/app/features/pet-informations/pet-form/pet-form.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ShelterNameValidatorDirective } from '../../../shared/directives/shelter-name-validator-directive';
 
 @Component({
   selector: 'app-pet-form',
-  imports: [FormsModule],
+  imports: [FormsModule, ShelterNameValidatorDirective],
   templateUrl: './pet-form.html',
   styleUrl: './pet-form.css',
 })
