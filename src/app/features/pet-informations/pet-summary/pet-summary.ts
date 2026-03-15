62ac7144423de66925f721f194e87486db4c3b23
feelipefreitas <feelipe.freitas97@gmail.com>
2026-03-14 21:38:12 -0300

    Mostrando informações do pet

--- src/app/features/pet-informations/pet-summary/pet-summary.ts
+++ src/app/features/pet-informations/pet-summary/pet-summary.ts
@@ -1,15 +1,13 @@
-import { JsonPipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { IPetData } from '../../../shared/models/pet-data';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pet-summary',
-  imports: [JsonPipe],
+  imports: [RouterLink],
   templateUrl: './pet-summary.html',
   styleUrl: './pet-summary.css',
 })
 export class PetSummary {
   petData = signal<IPetData>(history.state.petData);
-
-  especie = signal('dog');
 }
