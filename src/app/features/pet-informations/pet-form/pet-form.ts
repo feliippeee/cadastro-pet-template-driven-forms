487f1ffb9a3c04783a04d41467e9cf4aacfea472
feelipefreitas <feelipe.freitas97@gmail.com>
2026-03-14 21:09:42 -0300

    Redirecionando para tela de resumo

--- src/app/features/pet-informations/pet-form/pet-form.ts
+++ src/app/features/pet-informations/pet-form/pet-form.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { ShelterNameValidatorDirective } from '../../../shared/directives/shelter-name-validator-directive';
+import { Router } from '@angular/router';
+import { IPetData } from '../../../shared/models/pet-data';
 
 @Component({
   selector: 'app-pet-form',
@@ -8,7 +10,17 @@ import { ShelterNameValidatorDirective } from '../../../shared/directives/shelte
   templateUrl: './pet-form.html',
   styleUrl: './pet-form.css',
 })
-export class PetForm {}
+export class PetForm {
+  private readonly _router = inject(Router);
+
+  submitForm(form: NgForm) {
+    if (form.status === 'VALID') {
+      this._router.navigate(['/pet-summary'], {
+        state: { petData: form.value as IPetData },
+      });
+    }
+  }
+}
 
 // {
 //   petName: string;
