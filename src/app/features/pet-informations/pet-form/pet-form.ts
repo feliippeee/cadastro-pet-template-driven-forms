b387f2fb455e179dff5c2dc5e8d20d8f20581624
vhraposo <victorhcraposo@gmail.com>
2026-04-10 00:29:31 -0300

    1.2 - Definindo o objeto final do formulário

--- src/app/features/pet-informations/pet-form/pet-form.ts
+++ src/app/features/pet-informations/pet-form/pet-form.ts
@@ -7,3 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './pet-form.css',
 })
 export class PetForm {}
+
+// {
+//   petName: string;
+//   species: string;
+//   age: number;
+//   vaccinated: boolean;
+//   size: 'small' | 'medium' | 'large';
+//   temperament: {
+//     docile: boolean;
+//     active: boolean;
+//     protective: boolean;
+//   };
+//   shelterInfo: {
+//     shelterName: string;
+//     city: string;
+//   };
+//   bio: string;
+// }
