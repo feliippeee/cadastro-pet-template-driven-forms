03e60eb343978195d71080a8dd5e39120b5f4cd4
vhraposo <victorhcraposo@gmail.com>
2026-04-10 01:20:31 -0300

    1.2 - Redirecionando para tela de resumo

--- src/app/features/pet-informations/pet-summary/pet-summary.ts
+++ src/app/features/pet-informations/pet-summary/pet-summary.ts
@@ -1,11 +1,15 @@
+import { JsonPipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { IPetData } from '../../../shared/models/pet-data';
 
 @Component({
   selector: 'app-pet-summary',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './pet-summary.html',
   styleUrl: './pet-summary.css',
 })
 export class PetSummary {
+  petData = signal<IPetData>(history.state.petData);
+
   especie = signal('dog');
 }
