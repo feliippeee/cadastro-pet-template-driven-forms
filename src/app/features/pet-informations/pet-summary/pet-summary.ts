ca8f01b5d2ee3b67566fe01364d7d053fead2e26
vhraposo <victorhcraposo@gmail.com>
2026-04-10 01:25:33 -0300

    1.3 - Mostrando informações do pet

--- src/app/features/pet-informations/pet-summary/pet-summary.ts
+++ src/app/features/pet-informations/pet-summary/pet-summary.ts
@@ -1,15 +1,13 @@
-import { JsonPipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { IPetData } from '../../../shared/models/pet-data';
 
 @Component({
   selector: 'app-pet-summary',
-  imports: [JsonPipe],
+  imports: [RouterLink],
   templateUrl: './pet-summary.html',
   styleUrl: './pet-summary.css',
 })
 export class PetSummary {
   petData = signal<IPetData>(history.state.petData);
-
-  especie = signal('dog');
 }
